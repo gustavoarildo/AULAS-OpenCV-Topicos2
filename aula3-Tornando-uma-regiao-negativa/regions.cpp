536e9243f05574e26800e187a688ba63caa6e96a
gustavoarildo <gustavoarildo@gmail.com>
2020-08-26 12:42:20 -0300

    #3 segunda atividade parte3 pixels negativados

--- aula3-Tornando-uma-regiao-negativa/regions.cpp
+++ aula3-Tornando-uma-regiao-negativa/regions.cpp
@@ -7,44 +7,30 @@ using namespace std;
 
 int main(int argc, char** argv) {
   Mat image;
-  Vec3b val;
-  int ponto1x;
+  //Vec3b val;
   int ponto1y;
-  int ponto2x;
+  int ponto1x;
   int ponto2y;
-  ponto1x = atoi(argv[1]);
-  ponto1y = atoi(argv[2]);
-  ponto2x = atoi(argv[3]);
-  ponto2y = atoi(argv[4]);
+  int ponto2x;
+  ponto1y = atoi(argv[1]);
+  ponto1x = atoi(argv[2]);
+  ponto2y = atoi(argv[3]);
+  ponto2x = atoi(argv[4]);
 
   image = imread("biel.png", CV_LOAD_IMAGE_GRAYSCALE);
   if (!image.data)
     cout << "Erro ao abrir o arquivo biel.png" << endl;
 
   namedWindow("janela", WINDOW_AUTOSIZE);
-
+//os nomes estão como Y e X para evitar confusoes(em mim), estou ciente do sistema referencial do tipo destrógiro.
   for (int y=ponto1y; y < ponto2y; y++) {
     for (int x=ponto1x; x < ponto2x; x++) {
-      image.at<uchar>(y,x)=0;
+      image.at<uchar>(y,x)= 255 - image.at<uchar>(y,x);
     }
   }
 
   imshow("janela", image);
   waitKey();
 
-  image = imread("biel.png", CV_LOAD_IMAGE_COLOR);
-
-  val[0] = 0;   //B
-  val[1] = 0;   //G
-  val[2] = 255; //R
-
-  for (int i=200; i < 210; i++){
-    for (int j=10; j < 200; j++){
-      image.at<Vec3b>(i,j) = val;
-    }
-  }
-
-  imshow("janela", image);
-  waitKey();
   return 0;
 }

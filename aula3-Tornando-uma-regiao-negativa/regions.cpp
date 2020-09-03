87dd7304d79abbd6bf500451c0f411dd4ce5d171
gustavoarildo <gustavoarildo@gmail.com>
2020-09-03 14:23:50 -0300

    #5 segunda atividade parte5 COMENTADO e entregue na data

--- aula3-Tornando-uma-regiao-negativa/regions.cpp
+++ aula3-Tornando-uma-regiao-negativa/regions.cpp
@@ -1,31 +1,34 @@
+//Aluno: Gustavo Arildo Felix, Topicos 2 de informatica, atividade 2
+
 #include <iostream>
 #include <opencv2/opencv.hpp>
-#include <cstdlib>
+#include <cstdlib>//para o uso de atoi
 
 using namespace cv;
 using namespace std;
 
-int main(int argc, char** argv) {
+int main(int argc, char** argv) {//As entradas são atraves de argumentos do terminal, ex ./regions 100 100 250 200
   Mat image;
-  //Vec3b val;
+ 
   int ponto1y;
   int ponto1x;
   int ponto2y;
   int ponto2x;
-  ponto1y = atoi(argv[1]);
+  ponto1y = atoi(argv[1]);//converte a entrada para inteiro com a funcao atoi
   ponto1x = atoi(argv[2]);
   ponto2y = atoi(argv[3]);
   ponto2x = atoi(argv[4]);
 
   image = imread("biel.png", CV_LOAD_IMAGE_GRAYSCALE);
-  if (!image.data)
+  if (!image.data)// se não foi possivel abrir a imagem erro
     cout << "Erro ao abrir o arquivo biel.png" << endl;
 
   namedWindow("janela", WINDOW_AUTOSIZE);
 //os nomes estão como Y e X para evitar confusoes(em mim), estou ciente do sistema referencial do tipo destrógiro.
+//os laços vão acessar os pixels atraves das posiçoes x e y informadas
   for (int y=ponto1y; y < ponto2y; y++) {//que é X na verdade
     for (int x=ponto1x; x < ponto2x; x++) {//que é Y na verdade
-      image.at<uchar>(y,x)= 255 - image.at<uchar>(y,x);
+      image.at<uchar>(y,x)= 255 - image.at<uchar>(y,x); //branco menos imagem é o negativo da imagem
     }
   }
 
--- aula4-trocando-quadrantes-em-diagonal/trocaregioes.cpp
+++ aula4-trocando-quadrantes-em-diagonal/trocaregioes.cpp
@@ -1,3 +1,5 @@
+//Aluno: Gustavo Arildo Felix, Topicos 2 de informatica, atividade 2
+
 #include <iostream>
 #include <opencv2/opencv.hpp>
 
@@ -12,29 +14,29 @@ int main(int argc, char** argv) {
 
   image = imread("biel.png", CV_LOAD_IMAGE_GRAYSCALE);
   if (!image.data)
-    cout << "Erro ao abrir o arquivo biel.png" << endl;
+    cout << "Erro ao abrir o arquivo biel.png" << endl;// informa erro ao abrir imagem
   namedWindow("janela", WINDOW_AUTOSIZE);
 
   Mat image2;
-  image2 = image.clone();//necessario para auxiliar na montagem
+  image2 = image.clone();//necessario para auxiliar na montagem, a imagem dois é o alvo da montagem que não muda a original
 
 
-  alturaBiel = image.size().height;
-  larguraBiel = image.size().width;
+  alturaBiel = image.size().height;//recebe altura da imagem
+  larguraBiel = image.size().width;//recebe largura da imagem
 
-  Mat quadrante1 = image(Rect(0, 0, (larguraBiel/2), (alturaBiel/2)));//replicado ok//retangulo sem perda
-  Mat quadrante2 = image(Rect((larguraBiel/2), 0, (larguraBiel/2), (alturaBiel/2)));//replicado ok
-  Mat quadrante3 = image(Rect(0,(alturaBiel/2),(larguraBiel/2),(alturaBiel/2)));//replicado ok
-  Mat quadrante4 = image(Rect((larguraBiel/2),(alturaBiel/2),(larguraBiel/2),(alturaBiel/2)));//replicado ok// todos quadrantes replicados abaixo
+  Mat quadrante1 = image(Rect(0, 0, (larguraBiel/2), (alturaBiel/2)));//copia um quadrante da imagem,em forma retangular na posiçao especificada, quadrante superior esquerdo no caso
+  Mat quadrante2 = image(Rect((larguraBiel/2), 0, (larguraBiel/2), (alturaBiel/2)));
+  Mat quadrante3 = image(Rect(0,(alturaBiel/2),(larguraBiel/2),(alturaBiel/2)));
+  Mat quadrante4 = image(Rect((larguraBiel/2),(alturaBiel/2),(larguraBiel/2),(alturaBiel/2)));
 
-  quadrante1.copyTo(image2(Rect((larguraBiel/2),(alturaBiel/2),(larguraBiel/2),(alturaBiel/2))));//OK//definitivo baixo direita
-	quadrante2.copyTo(image2(Rect(0,(alturaBiel/2),(larguraBiel/2),(alturaBiel/2))));//OK//definitivo baixo esquerda
-	quadrante3.copyTo(image2(Rect((larguraBiel/2), 0,(larguraBiel/2),(alturaBiel/2))));//OK//definitivo cima direita
-	quadrante4.copyTo(image2(Rect(0, 0, (larguraBiel/2), (alturaBiel/2))));//OK//definitivo cima esquerda
+  quadrante1.copyTo(image2(Rect((larguraBiel/2),(alturaBiel/2),(larguraBiel/2),(alturaBiel/2))));// baixo direita, imagem dois vai receber todos os quadrantes nela
+	quadrante2.copyTo(image2(Rect(0,(alturaBiel/2),(larguraBiel/2),(alturaBiel/2))));//baixo esquerda
+	quadrante3.copyTo(image2(Rect((larguraBiel/2), 0,(larguraBiel/2),(alturaBiel/2))));//cima direita
+	quadrante4.copyTo(image2(Rect(0, 0, (larguraBiel/2), (alturaBiel/2))));//cima esquerda
 
 
-  //imshow("janela", quadrante1);//necessario para testar quadrantes
-  imshow("janela", image2);
+  //imshow("janela", quadrante1);//necessario para testar quadrantes, no desenvolvimento
+  imshow("janela", image2);//exibe a imagem remontada
   waitKey();
 
   return 0;

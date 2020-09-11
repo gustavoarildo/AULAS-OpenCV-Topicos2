5b2faba6bf183b990f16663da34c1128cac89b0e
gustavoarildo <gustavoarildo@gmail.com>
2020-09-10 22:19:00 -0300

    #14 terceira atividade parte9 talvez pronta

--- aula5-Preenchendo-regiões/atividade3-Preenchendo-regiões/exemploPlus255obj/labeling.cpp
+++ aula5-Preenchendo-regiões/atividade3-Preenchendo-regiões/exemploPlus255obj/labeling.cpp
@@ -10,10 +10,10 @@ int main(int argc, char **argv)
 {
   Mat image, mask;
   int width, height;
-  int nobjects;
-  Vec3b valROTULA; //Uso de matriz rgb para superar 255 objetos
-  Vec3b valBRANCO; //cor branca
-  vector<vector<int>> pilhaVec3b;//guardar as cores
+  int nobjects;                   //o numero de objetos brancos na imagem//que são 32, testado com o exemplo original
+  Vec3b valROTULA;                //Uso de matriz rgb para superar 255 objetos
+  Vec3b valBRANCO;                //cor branca
+  vector<vector<int>> pilhaVec3b; //guardar todas as cores
 
   CvPoint p;
   image = imread(argv[1], CV_LOAD_IMAGE_COLOR); //cores RGB = 255*255*255 = mais de 16 milhões
@@ -37,35 +37,24 @@ int main(int argc, char **argv)
   valROTULA[1] = 0; //GREEN//VERDE
   valROTULA[2] = 0; //RED//VERMELHO
 
-  for (int i = 1; i < 255; i++)
+  for (int i = 1; i < 255; i += 1) //vai passar por todas as cores possiveis com 8 bits
   {
-    for (int j = 1; j < 255; j++)
+    for (int j = 1; j < 255; j += 1) //inicia com 1 para não alterar o preto 0
     {
-
-      for (int k = 1; k < 255; k++)
+      for (int k = 1; k < 255; k += 1) //uma matriz é usada como pilha de armazenamento
       {
-        //valROTULA[0] = i; //BLUE//AZUL
-        //valROTULA[1] = j; //GREEN//VERDE
-        //valROTULA[2] = k; //RED//VERMELHO
 
-        //pilhaVec3b.push_back(valROTULA);
-        vector<int> v1;
-         v1.push_back(i);
-         v1.push_back(j);
-         v1.push_back(k);
+        vector<int> v1; //esse vetor recebe as 3 cores rgb combinadas
+        v1.push_back(i);
+        v1.push_back(j);
+        v1.push_back(k);
 
-        //pilhaVec3b.push_back(i);
-        //pilhaVec3b.push_back(j);
-        //pilhaVec3b.push_back(k);
-
-        pilhaVec3b.push_back(v1);
-
-        //put vairotula
+        pilhaVec3b.push_back(v1); //o vetor é inserido na matriz com seus valores
       }
     }
   }
 
-  // Busca objetos com buracos
+  // Busca objetos
   nobjects = 0;
   for (int i = 0; i < height; i++)
   {
@@ -73,32 +62,36 @@ int main(int argc, char **argv)
     {
       if (image.at<Vec3b>(i, j) == valBRANCO)
       {
-        // Encontrou um objeto
-        nobjects++;
-        p.x = j;
-        p.y = i;
 
         //UTILIZANDO AS CORES
-        if (!pilhaVec3b.empty())
+        if (!pilhaVec3b.empty()) //se existirem cores disponiveis na pilha
         {
-          int tamanhoPilha = pilhaVec3b.size();
-          valROTULA[0] = pilhaVec3b[tamanhoPilha].size(); //BLUE//AZUL
-          pilhaVec3b.pop_back();
-          valROTULA[1] = pilhaVec3b[tamanhoPilha].size(); //GREEN//VERDE
-          pilhaVec3b.pop_back();
-          valROTULA[2] = pilhaVec3b[tamanhoPilha].size(); //RED//VERMELHO
-          pilhaVec3b.pop_back();
+          // Encontrou um objeto
+          nobjects++; //vai contar o objeto encontrado
+          p.x = j;
+          p.y = i;
 
-          floodFill(image, p, valROTULA);
+          int tamanhoPilha = pilhaVec3b.size();           //usa-se o topo da pilha para preencher um vec3b
+          valROTULA[0] = pilhaVec3b[tamanhoPilha].size(); //BLUE//AZUL
+          pilhaVec3b.pop_back();                          //quando um valor é utilizado ele sai da pilha
+          valROTULA[1] = pilhaVec3b[tamanhoPilha].size(); //GREEN//VERDE
+          pilhaVec3b.pop_back();                          //quando não existirem mais cores o else é ativado
+          valROTULA[2] = pilhaVec3b[tamanhoPilha].size(); //RED//VERMELHO
+          pilhaVec3b.pop_back();                          //a combinação das cores é usada como rotulo
+
+          floodFill(image, p, valROTULA); //o objeto é rotulado
         }
-        else{printf("acabaram as cores\n");}
+        else
+        {
+          printf("acabaram as cores\n"); //se existirem mais objetos que cores
+        }
       }
     }
   }
 
-  printf("%d é a quantidade de objetos existentes\n", nobjects);
+  printf("%d é a quantidade de objetos existentes\n", nobjects); //numero de objetos brancos rotulados
 
-  imshow("image", image);
+  imshow("image", image); //geralmente a imagem é bem apagada, mas rotula bem
   imwrite("labeling.png", image);
   waitKey();
   return 0;

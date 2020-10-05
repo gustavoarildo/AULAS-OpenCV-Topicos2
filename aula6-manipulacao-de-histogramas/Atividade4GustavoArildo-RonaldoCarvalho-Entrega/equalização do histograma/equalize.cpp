bf613d9886aed948af4c05fcda4e907c7c8451cd
gustavoarildo <gustavoarildo@gmail.com>
2020-10-05 10:59:52 -0300

    #19 quinta atividade parte1 exemplo funcionando

--- aula6-manipulacao-de-histogramas/Atividade4GustavoArildo-RonaldoCarvalho-Entrega/equalização do histograma/equalize.cpp
+++ aula6-manipulacao-de-histogramas/Atividade4GustavoArildo-RonaldoCarvalho-Entrega/equalização do histograma/equalize.cpp
@@ -1,38 +1,41 @@
 //Alunos: Gustavo Arildo Felix e Ronaldo Carvalho, Topicos 2 de informatica, atividade 4
 #include <iostream>
 #include <opencv2/opencv.hpp>
-//#include <opencv2/imgcodecs/legacy/constants_c.h>
+//#include <opencv2/imgcodecs/legacy/constants_c.h>//não é necessario este include
+//ele pode gerar erros fatais
 
 using namespace cv;
 using namespace std;
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
   Mat image;
   Mat histR, histG, histB;
+  Mat imageEqualizada;
 
   VideoCapture cap;
   vector<Mat> planes;
+  vector<Mat> canais;
 
   float range[] = {0, 256};
-  const float *histrange = { range };
+  const float *histrange = {range};
 
   int width, height;
-  int nbins = 64, key = 0;
+  int nbins = 64, key = 0; /*, key = 0//serve para dar warning, deve haver outras alternativas*/
   int histw, histh;
-  int somaVermelho;
-  int somaVermelhoAntigo;
 
   bool uniform = true;
   bool acummulate = false;
 
-  cap.open(0);			// Dispositivo com identificador 0
+  cap.open(0); // Dispositivo com identificador 0
 
-  if (!cap.isOpened()) {
+  if (!cap.isOpened())
+  {
     cout << "Erro de acesso, cameras indisponiveis";
     return -1;
   }
 
-  width  = cap.get(CV_CAP_PROP_FRAME_WIDTH);
+  width = cap.get(CV_CAP_PROP_FRAME_WIDTH);
   height = cap.get(CV_CAP_PROP_FRAME_HEIGHT);
 
   cout << "largura = " << width << endl;
@@ -44,9 +47,9 @@ int main(int argc, char** argv) {
   Mat histImgR(histh, histw, CV_8UC3, Scalar(0, 0, 0));
   Mat histImgG(histh, histw, CV_8UC3, Scalar(0, 0, 0));
   Mat histImgB(histh, histw, CV_8UC3, Scalar(0, 0, 0));
-  Mat vermelhoAntigo(histh, histw, CV_8UC3, Scalar(0, 0, 0));
 
-  while (1) {
+  while (1)
+  {
     cap >> image;
     split(image, planes);
     calcHist(&planes[0], 1, 0, Mat(), histR, 1,
@@ -67,48 +70,39 @@ int main(int argc, char** argv) {
     histImgG.setTo(Scalar(0));
     histImgB.setTo(Scalar(0));
 
-    for (int i=0; i < nbins; i++) {
+    for (int i = 0; i < nbins; i++)
+    {
       line(histImgR,
            Point(i, histh),
-           Point(i, histh-cvRound(histR.at<float>(i))),
+           Point(i, histh - cvRound(histR.at<float>(i))),
            Scalar(0, 0, 255), 1, 8, 0);
       line(histImgG,
            Point(i, histh),
-           Point(i, histh-cvRound(histG.at<float>(i))),
+           Point(i, histh - cvRound(histG.at<float>(i))),
            Scalar(0, 255, 0), 1, 8, 0);
       line(histImgB,
            Point(i, histh),
-           Point(i, histh-cvRound(histB.at<float>(i))),
+           Point(i, histh - cvRound(histB.at<float>(i))),
            Scalar(255, 0, 0), 1, 8, 0);
     }
-    histImgR.copyTo(image(Rect(0, 0        , nbins, histh)));
-    histImgG.copyTo(image(Rect(0, histh    , nbins, histh)));
+    histImgR.copyTo(image(Rect(0, 0, nbins, histh)));
+    histImgG.copyTo(image(Rect(0, histh, nbins, histh)));
     histImgB.copyTo(image(Rect(0, 2 * histh, nbins, histh)));
-
-
-
-    for(int i = 0; i<histh; i++){//vermelho filmado é somado na imagem atual e na anterior
-      for (int j = 0; j<histw; j++){//se uma tiver mais vermelho que a outra, algo mudou/moveu
-        somaVermelho = somaVermelho + histImgR.at<uchar>(i, j);
-        somaVermelhoAntigo = somaVermelhoAntigo + vermelhoAntigo.at<uchar>(i,j);
-      }
-    }
-    if(abs(somaVermelho - somaVermelhoAntigo)>=4000){//comparados os valores de vermelho
-      putText(image, "ALGO SE MOVEU!", cvPoint(0,470),//se a diferença supera 8000
-      CV_FONT_NORMAL,2.0,cvScalar(250,0,0),1,CV_AA);//o alarme ALGO SE MOVEU aparece
-
-    }
-
-
-
-
-
     imshow("image", image);
+
+    //AREA ADICIONADA AO EXEMPLO BEGIN
+    split(image, canais);               //divide os canais da imagem
+    equalizeHist(canais[0], canais[0]); //os canais são equalizados
+    equalizeHist(canais[1], canais[1]);
+    equalizeHist(canais[2], canais[2]);
+    merge(canais, imageEqualizada);             //os canais agora equalizados realizam a fusão
+    imshow("imageEqualizada", imageEqualizada); //a imagem equalizada é exibida
+
+    //AREA ADICIONADA AO EXEMPLO END
+
     key = waitKey(30);
-    // if (waitKey(30) >= 0) break;
-    vermelhoAntigo = histImgR.clone();//Imagem é capturada para futura comparação
-    somaVermelho=0;
-    somaVermelhoAntigo=0;//valores zerados para que continue o ciclo sem fim
+    //  if (waitKey(30) >= 0) break;
+    //waitKey();
   }
 
   return 0;
